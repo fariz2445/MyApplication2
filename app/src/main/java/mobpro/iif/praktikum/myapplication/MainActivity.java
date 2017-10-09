9fe4be7e2ec67aad0de87936ec57f3513237e861
fariz2445 <farizalfarisi80@gmail.com>
2017-10-09 17:34:37 +0700

    Menambahkan mainactivity.java dan halo fariz pada activity_main.xml

--- app/src/main/java/mobpro/iif/praktikum/myapplication/MainActivity.java
+++ app/src/main/java/mobpro/iif/praktikum/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package mobpro.iif.praktikum.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView teks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teks = (TextView) findViewById(R.id.teks);
     }
 }
